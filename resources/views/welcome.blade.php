40658a719bdb05d068ef15bd15246f86d64697bd
Jorge Diaz <jorge.diaz@unibague.edu.co>
2017-05-21 18:53:14 +0000

    Cambio: cambio de idioma

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,10 +5,13 @@
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
-                <div class="panel-heading">Welcome</div>
+                <div class="panel-heading">Bienvenido</div>
 
                 <div class="panel-body">
-                    Your Application's Landing Page.
+                    <p>
+                        Este sitio web, permite visualizar las copias de seguridad realizadas por medio de las tareas programadas de windows,
+                        con la aplicación dispuesta para ello.
+                    </p>
                 </div>
             </div>
         </div>
