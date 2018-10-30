ab0f4db48d0f3ae4d14daeaea61becf49f7966a4
Jorge Diaz <crafstmancloud@gmail.com>
2018-10-29 21:59:46 -0500

    UPDATE correcion en el menu

--- resources/views/help/index.blade.php
+++ resources/views/help/index.blade.php
@@ -19,9 +19,12 @@
                 <div class="panel-heading panel-heading-divider">Ayuda<span class="panel-subtitle">¿Necesitas ayuda para manejar la aplicación?.</span></div>
                 <div class="panel-body">
                     <div class="panel-body">
-                        <a href="">¿Como descargar su copia de seguridad?</a>
-                        <a href="">¿C&oacute;mo cambio mi contrase&ntilde;a de seguridad?</a>
-                        <a href="">¿C&oacute;mo creo un usuario nuevo?</a>
+                        <ul>
+                            <li><a href="">¿Como descargo mi copia de seguridad?</a></li>
+                            <li><a href="">¿C&oacute;mo cambio mi contrase&ntilde;a de seguridad?</a></li>
+                            <li><a href="">¿C&oacute;mo creo un usuario nuevo?</a></li>
+                            <li><a href="">¿C&oacute;mo elimino mi copia de seguridad?</a></li>
+                        </ul>
                     </div>
                 </div>
             </div>
